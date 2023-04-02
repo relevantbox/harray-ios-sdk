e5f11d7095eff09a54fd625ea3c2ab5e0161bc92
Yildirim Adiguzel <yadiguzel@gmail.com>
2023-04-01 21:47:02 -0700

    in app notification enchancements

--- harray-ios-sdk/context/ApplicationContextHolder.swift
+++ harray-ios-sdk/context/ApplicationContextHolder.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class ApplicationContextHolder {
     private let persistentId: String
-    private let sdkVersion = "3.1.2"
+    private let sdkVersion = "3.1.3"
     private var newInstallation = false
 
     init(userDefaults: UserDefaults) {
